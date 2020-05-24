7652f4c93390e83bc451ec3533b723267a3cbebe
small-rose <970175021@qq.com>
2020-05-24 11:42:00 +0800

    Site updated: 2020-05-24 11:41:56

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,3 +1,3 @@
-// build time:Sun May 24 2020 11:35:46 GMT+0800 (中国标准时间)
+// build time:Sun May 24 2020 11:41:47 GMT+0800 (中国标准时间)
 $(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
 //rebuild by neat 
