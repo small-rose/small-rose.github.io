5fb3ad9c8e7c7e5ecfc51921f2177f9681f51a79
small-rose <970175021@qq.com>
2020-06-27 23:39:03 +0800

    Site updated: 2020-06-27 23:38:53

--- webpushr-sw.js
+++ webpushr-sw.js
@@ -1,3 +1,1 @@
-// build time:Sat Jun 27 2020 23:19:16 GMT+0800 (中国标准时间)
-importScripts("https://cdn.webpushr.com/sw-server.min.js");
-//rebuild by neat 
+importScripts('https://cdn.webpushr.com/sw-server.min.js');
