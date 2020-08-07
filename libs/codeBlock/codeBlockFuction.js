da26c7e414fd3b9da7f474ef83a5ebd2b01f09b3
small-rose <970175021@qq.com>
2020-08-07 11:39:30 +0800

    Site updated: 2020-08-07 11:39:10

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,5 @@
-// build time:Wed Aug 05 2020 20:24:54 GMT+0800 (中国标准时间)
-$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
-//rebuild by neat 
+// 代码块功能依赖
+
+$(function () {
+    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
+});
