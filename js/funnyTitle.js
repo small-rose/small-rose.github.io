b4413ae952870dbf8c2f3e2bfa484e62ffac8780
small-rose <970175021@qq.com>
2020-08-27 22:00:22 +0800

    Site updated: 2020-08-27 22:00:02

--- js/funnyTitle.js
+++ js/funnyTitle.js
@@ -1,3 +1,3 @@
-// build time:Thu Aug 27 2020 21:37:58 GMT+0800 (中国标准时间)
+// build time:Thu Aug 27 2020 21:59:50 GMT+0800 (中国标准时间)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","https://cdn.jsdelivr.net/gh/small-rose/small-rose.github.io@master/medias/funny.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","https://cdn.jsdelivr.net/gh/small-rose/small-rose.github.io@master/medias/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好啦 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Thu Aug 27 2020 21:37:58 GMT+0800 (中国标准时间)
+// build time:Thu Aug 27 2020 21:59:51 GMT+0800 (中国标准时间)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 
