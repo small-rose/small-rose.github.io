208d243a8807fa34c70ce4a8a7a1b54041fb2c39
small-rose <970175021@qq.com>
2020-09-21 18:20:08 +0800

    Site updated: 2020-09-21 18:19:50

--- js/funnyTitle.js
+++ js/funnyTitle.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 21 2020 13:49:21 GMT+0800 (中国标准时间)
+// build time:Mon Sep 21 2020 18:19:07 GMT+0800 (中国标准时间)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","https://cdn.jsdelivr.net/gh/small-rose/small-rose.github.io@master/medias/funny.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","https://cdn.jsdelivr.net/gh/small-rose/small-rose.github.io@master/medias/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好啦 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 21 2020 13:49:22 GMT+0800 (中国标准时间)
+// build time:Mon Sep 21 2020 18:19:07 GMT+0800 (中国标准时间)
 $(function(){var e=$('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');$(".code-area").prepend(e);$(".code-expand").on("click",function(){if($(this).parent().hasClass("code-closed")){$(this).siblings("pre").find("code").show();$(this).parent().removeClass("code-closed")}else{$(this).siblings("pre").find("code").hide();$(this).parent().addClass("code-closed")}})});
 //rebuild by neat 
