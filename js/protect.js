233cdf8f56c80a8258098a7b60947a1ed4e0a53d
small-rose <small-rose@qq.com>
2021-01-03 18:24:10 +0800

    Site updated: 2021-01-03 18:23:44

---
+++ js/protect.js
@@ -0,0 +1,3 @@
+// build time:Sun Jan 03 2021 18:20:24 GMT+0800 (中国标准时间)
+document.onkeydown=function(){var e=window.event||arguments[0];if(e.keyCode==123){alert("请尊重劳动成果！");return false}else if(e.ctrlKey&&e.shiftKey&&e.keyCode==73){alert("请尊重劳动成果！");return false}else if(e.ctrlKey&&e.keyCode==85){alert("请尊重劳动成果！");return false}else if(e.ctrlKey&&e.keyCode==83){alert("请尊重劳动成果！");return false}};document.oncontextmenu=function(){alert("请尊重劳动成果！");return false};
+//rebuild by neat 
--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Sun Jan 03 2021 17:27:54 GMT+0800 (中国标准时间)
+// build time:Sun Jan 03 2021 18:20:25 GMT+0800 (中国标准时间)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 
