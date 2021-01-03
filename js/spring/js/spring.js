6b6fb9ad9a5b6beee841d715b794f771e3048664
small-rose <small-rose@qq.com>
2021-01-03 17:07:58 +0800

    Site updated: 2021-01-03 17:07:20

---
+++ js/spring/js/spring.js
@@ -0,0 +1,20 @@
+
+// 网站主题切换模式开始
+function switchThemes() {
+    var body = document.body;
+    if(body.classList.contains('themespring')){
+      document.body.classList.remove('themespring');
+      localStorage.setItem('themespring','0');
+      return;
+    } else {
+      document.body.classList.add('themespring');
+      localStorage.setItem('themespring','1');
+      return;
+    }
+  };
+// 网站主题切换模式结束
+
+function autoSpring(){
+    document.body.classList.add('themespring');
+    localStorage.setItem('themespring','1');
+}
--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Sun Jan 03 2021 01:18:10 GMT+0800 (中国标准时间)
+// build time:Sun Jan 03 2021 17:04:51 GMT+0800 (中国标准时间)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 
