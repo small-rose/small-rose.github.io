94c5d2a8cf10d9496c8f0a7decf2e693d1bf648b
small-rose <small-rose@qq.com>
2021-01-28 11:51:45 +0800

    Site updated: 2021-01-28 11:51:13

--- js/protect.js
+++ js/protect.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 26 2021 20:15:24 GMT+0800 (中国标准时间)
+// build time:Thu Jan 28 2021 11:47:29 GMT+0800 (中国标准时间)
 document.onkeydown=function(){var e=window.event||arguments[0];if(e.keyCode==123){window.location.href="https://zhangxiaocai.cn?f=reboot";return false}else if(e.ctrlKey&&e.shiftKey&&e.keyCode==73){window.location.href="https://zhangxiaocai.cn?f=reboot";return false}else if(e.ctrlKey&&e.keyCode==85){window.location.href="https://zhangxiaocai.cn?f=reboot";return false}else if(e.ctrlKey&&e.keyCode==83){window.location.href="https://zhangxiaocai.cn?f=reboot";return false}};document.oncontextmenu=function(){window.location.href="https://chat.zhangxiaocai.cn";return false};
 //rebuild by neat 
--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 26 2021 20:15:25 GMT+0800 (中国标准时间)
+// build time:Thu Jan 28 2021 11:47:29 GMT+0800 (中国标准时间)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 
