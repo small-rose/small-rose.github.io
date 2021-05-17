780dc0e1df567e5a9c493d78690761afd45ec556
small-rose <small-rose@qq.com>
2021-05-18 00:33:02 +0800

    Site updated: 2021-05-18 00:32:17

--- js/protect.js
+++ js/protect.js
@@ -1,3 +1,3 @@
-// build time:Sun May 09 2021 18:19:08 GMT+0800 (中国标准时间)
+// build time:Tue May 18 2021 00:32:09 GMT+0800 (中国标准时间)
 document.onkeydown=function(){var e=window.event||arguments[0];if(e.keyCode==123){window.location.href="https://zhangxiaocai.cn?f=reboot";return false}else if(e.ctrlKey&&e.shiftKey&&e.keyCode==73){window.location.href="https://zhangxiaocai.cn?f=reboot";return false}else if(e.ctrlKey&&e.keyCode==85){window.location.href="https://zhangxiaocai.cn?f=reboot";return false}else if(e.ctrlKey&&e.keyCode==83){window.location.href="https://zhangxiaocai.cn?f=reboot";return false}};document.oncontextmenu=function(){window.location.href="https://chat.zhangxiaocai.cn";return false};
 //rebuild by neat 
