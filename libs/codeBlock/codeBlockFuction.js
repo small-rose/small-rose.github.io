2ed0c4696054c1e105bd613f24731c06d1347cb1
small-rose <small-rose@qq.com>
2024-01-27 19:24:31 +0800

    Site updated: 2024-01-27 19:23:44

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Wed Jul 05 2023 21:43:31 GMT+0800 (中国标准时间)
+// build time:Sat Jan 27 2024 19:00:59 GMT+0800 (中国标准时间)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 
--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,3 +1,3 @@
-// build time:Wed Jul 05 2023 21:43:31 GMT+0800 (中国标准时间)
+// build time:Sat Jan 27 2024 19:00:59 GMT+0800 (中国标准时间)
 $(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
 //rebuild by neat 
